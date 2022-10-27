bcdba63fef83148187f4e80d2bec80b40ccd67c9
Nicolas <nicolas.soares.gomes@gmail.com>
2022-10-26 23:39:10 -0300

    create singleton instance db and create schema db

--- server/src/main.go
+++ server/src/main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"real_quote_server/src/application/routes"
+	"real_quote_server/src/shared/config"
 
 	"github.com/gin-gonic/gin"
+	"github.com/jmoiron/sqlx"
 )
 
 func main() {
+
+	err := createSchema(config.Conn())
+	if err != nil {
+		panic(err)
+	}
+
 	app := gin.New()
 
 	app.Use(gin.Logger())
@@ -15,4 +23,13 @@ func main() {
 	routes.Routes(app)
 
 	app.Run(":8080")
+}
+
+func createSchema(db *sqlx.DB) error {
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS quote (bid INT NOT NULL)")
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- server/src/shared/config/database.go
+++ server/src/shared/config/database.go
@@ -1,20 +1,28 @@
 package config
 
 import (
+	"sync"
+
 	"github.com/jmoiron/sqlx"
 	_ "github.com/mattn/go-sqlite3"
 )
 
+var lock = &sync.Mutex{}
+var sqlxConn *sqlx.DB
+
 func Conn() *sqlx.DB {
-	db, err := sqlx.Open("sqlite3", ":memory:")
-	if err != nil {
-		panic(err)
+	if sqlxConn == nil {
+		lock.Lock()
+		defer lock.Unlock()
+		if sqlxConn == nil {
+			db, err := sqlx.Open("sqlite3", ":memory:")
+			if err != nil {
+				panic(err)
+			}
+
+			sqlxConn = db
+		}
 	}
 
-	_, err = db.Exec("CREATE TABLE IF NOT EXISTS quote (bid INT NOT NULL)")
-	if err != nil {
-		panic(err)
-	}
-
-	return db
+	return sqlxConn
 }
